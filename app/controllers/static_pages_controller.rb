670dc02584bf8c84022563594882d39aa43b2c69
keroken <kenichi.shinagawa@gmail.com>
2015-09-05 06:15:51 +0000

    add map to home view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,13 @@ class StaticPagesController < ApplicationController
   def home
     @micropost = current_user.microposts.build if logged_in?
     @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    
+    @locations = Location.all
+    @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+      marker.lat location.latitude
+      marker.lng location.longitude
+      marker.infowindow location.description
+      marker.json({title: location.title})
+    end
   end
 end
